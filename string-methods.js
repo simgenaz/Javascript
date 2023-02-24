52a84d18ebef13a60c81c630dff119e4843dbea1
Simgenaz Yalçınkaya <simge.naz.y@outlook.com>
2023-02-25 01:05:07 +0300

    string-methods-slice-trip-split

---
+++ string-methods.js
@@ -0,0 +1,25 @@
+let kursAdi = "  Komple Uygulamalı Web Geliştirme Eğitimi.";
+let sonuc;
+
+sonuc = kursAdi.toLowerCase();
+sonuc = kursAdi.toUpperCase();
+sonuc = kursAdi.length;
+sonuc = kursAdi[1];
+sonuc = kursAdi.slice(0,6);
+sonuc = kursAdi.slice(10);
+sonuc = kursAdi.slice(-10);
+sonuc = kursAdi.slice(-10,-5);
+
+sonuc = kursAdi.substring(0,6);
+sonuc = kursAdi.substring(10);
+
+sonuc = kursAdi.replace("Eğitim","Kursu");
+sonuc = kursAdi.trim(); 
+// trim  Boşlukları siler.
+
+sonuc = kursAdi.indexOf("Komple");
+sonuc = kursAdi.split(" ");
+sonuc = kursAdi.split(" ")[0];
+sonuc = kursAdi.split(" ")[3];
+
+console.log(sonuc);
